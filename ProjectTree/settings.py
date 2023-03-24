f90114b78acc376ba730e21a64ed7c43332ea947
Di Sk <disk@MacBook-Air-Di.Dlink>
2023-03-24 19:45:38 +0200

    for deploy 13

--- ProjectTree/settings.py
+++ ProjectTree/settings.py
@@ -112,8 +112,8 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [STATIC_DIR]
+# STATIC_DIR = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [STATIC_DIR]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
