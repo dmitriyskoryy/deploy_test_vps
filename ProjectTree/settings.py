7a3974ebb84d28946c5f5273d3ea1e143646486f
Di Sk <disk@MacBook-Air-Di.Dlink>
2023-03-24 14:08:10 +0200

    for deploy 7

--- ProjectTree/settings.py
+++ ProjectTree/settings.py
@@ -108,7 +108,7 @@ USE_TZ = True
 
 # STATIC_URL = 'static/'
 
-STATIC_ROOT = 'static/'
+STATIC_ROOT = 'static/root'
 
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [STATIC_DIR]
